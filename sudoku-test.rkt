44ec5da5123d7dceda64c0c81af6de496f8ee987
Alice <alicegutteridge@gmail.com>
2015-04-25 20:11:05 +0100

    column + test written. working on box.

--- sudoku-test.rkt
+++ sudoku-test.rkt
@@ -1,6 +1,20 @@
 #lang racket
-(require racket/include rackunit)
-(include "sudoku.rkt")
-(include "data.rkt")
+(require rackunit "sudoku.rkt" "data.rkt")
 
+;; Testing transform
 (check-equal? (transform example) transform-result)
+
+;; Testing column on grid with sets
+(check-equal? (column transform-result 0) (list 
+                                           (set (list 1 2 3 4 5 6 7 8 9))
+                                           1
+                                           (set (list 1 2 3 4 5 6 7 8 9))
+                                           (set (list 1 2 3 4 5 6 7 8 9))
+                                           6
+                                           (set (list 1 2 3 4 5 6 7 8 9))
+                                           (set (list 1 2 3 4 5 6 7 8 9))
+                                           (set (list 1 2 3 4 5 6 7 8 9))
+                                           (set (list 1 2 3 4 5 6 7 8 9))))
+
+;; Testing column on grid
+(check-equal? (column example 8) (list 0 0 0 0 9 0 0 3 0))
--- sudoku.rkt
+++ sudoku.rkt
@@ -1,19 +1,10 @@
 #lang racket
-(provide transform)
+(provide transform 
+         column)
 
 ;(define (solve matrix)(matrix))
 
-(define example (list (list 9 1 3 4 5 6 2 8 0)
-                      (list 0 9 0 0 0 0 0 0 0)
-                      (list 0 0 9 0 0 0 0 0 0)
-                      (list 0 0 0 0 0 0 0 0 0)
-                      (list 0 0 0 0 0 0 0 0 0)
-                      (list 0 0 0 0 0 0 0 0 0)
-                      (list 0 0 0 0 0 0 0 0 0)
-                      (list 0 0 0 0 0 0 0 0 0)
-                      (list 0 0 0 0 0 0 0 0 0)))
-
-; Takes a list of list of integers, changes any zeros to sets
+;; Takes a list of list of integers, changes any zeroes to sets
 (define (transform matrix)
   
   (define (transform-helper matrix acc)
@@ -23,6 +14,42 @@
        (cons (transform-helper (car matrix) acc) (transform-helper (cdr matrix) acc))]
       [(= 0 (car matrix)) 
        (cons (set '(1 2 3 4 5 6 7 8 9)) (transform-helper (cdr matrix) acc))]
-      [else (cons (car matrix) (transform-helper (cdr matrix) acc))]))
+      [else 
+       (cons (car matrix) (transform-helper (cdr matrix) acc))]))
   
   (transform-helper matrix '()))
+
+;; Extracts a column from a grid, 0-indexed
+(define (column matrix num)
+  
+  (define (column-helper matrix)
+    (cond
+      [(empty? matrix) '()]
+      [(list? (car matrix)) 
+       (cons (column-helper (car matrix)) (column-helper (cdr matrix)))] 
+      [else 
+       (list-ref matrix num)]))
+  
+  (column-helper matrix))
+
+;; Extracts a box from a grid, 0-indexed
+;; Boxes are numbered 0-8, top-left until bottom-right
+(define (box matrix num)
+ 
+  (define which-rows 0)  ; should this be let?
+  (define which-columns 0)
+  (define (rows matrix which-rows)
+    (cond 
+      [(= which-rows 0) '()]
+      [(< which-rows 4) 
+       (cons
+        (columns (car matrix) which-columns))
+        (rows (cdr matrix (- which-rows 1)))]
+      [else
+       (rows (cdr matrix (- which-rows 1)))]))
+  
+  (define (columns matrix which-columns)
+    '())
+  
+  (rows matrix 0)
+    )
